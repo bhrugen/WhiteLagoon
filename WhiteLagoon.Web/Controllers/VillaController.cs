0f7095d9cb1c4d51df4f0f42e63893ed3aab559c
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:24:00 -0500

    Section 3 - Empty View for Create Villa

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -17,5 +17,10 @@ namespace WhiteLagoon.Web.Controllers
             var villas = _db.Villas.ToList();
             return View(villas);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
