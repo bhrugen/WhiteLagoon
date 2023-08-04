5e889d6fc15385df06ed4ac185ad4de19e5a32ae
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:48:06 -0500

    Section 6 - ViewBag

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -28,7 +28,7 @@ namespace WhiteLagoon.Web.Controllers
                 Text = u.Name,
                 Value = u.Id.ToString()
             });
-            ViewData["VillaList"] = list;
+            ViewBag.VillaList = list;
             return View();
         }
 
