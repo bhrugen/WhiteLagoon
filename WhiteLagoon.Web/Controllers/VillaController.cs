b42d65cb6052dfb1eeda396ba2144f03a02ca52b
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:36:16 -0500

    Section 4 - Multiple ways to retrieve in EF Core

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -44,6 +44,8 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Update(int villaId)
         {
             Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            //Villa? obj = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);
             if (obj == null)
             {
                 return NotFound();
