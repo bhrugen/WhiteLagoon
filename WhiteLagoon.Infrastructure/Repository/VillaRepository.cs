1ce8f8f2447016e15cec437725fd07c10bc3b78d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-04 10:59:18 -0500

    Section 7 - Implement Villa Repository

---
+++ WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        public void Add(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Infrastructure.Data;
+using WhiteLagoon.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<IVillaRepository,VillaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
