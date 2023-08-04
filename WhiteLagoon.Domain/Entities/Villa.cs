1219d4e7a8ec4fdffa8016bb4ba0aed3c1d6133d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:12:52 -0500

    Section 2 - Some details on Migrations in EF Core

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -15,7 +15,7 @@ namespace WhiteLagoon.Domain.Entities
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
+        public DateTime? Created_Date { get; set; }
+        public DateTime? Updated_Date { get; set; }
     }
 }
---
+++ WhiteLagoon.Infrastructure/Migrations/20230804001243_ModfiyNamesInVillaTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModfiyNamesInVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Villas",
+                newName: "Updated_Date");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Villas",
+                newName: "Created_Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Updated_Date",
+                table: "Villas",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Created_Date",
+                table: "Villas",
+                newName: "CreatedDate");
+        }
+    }
+}
