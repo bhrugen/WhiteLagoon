7225ed43f54c0c55c07f17c2bf3cadb5ad08269f
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-04 13:16:58 -0500

    Section 8 - Add Form File in Villa Model

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,8 @@ namespace WhiteLagoon.Domain.Entities
         public int Sqft { get; set; }
         [Range(1, 10)]
         public int Occupancy { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
