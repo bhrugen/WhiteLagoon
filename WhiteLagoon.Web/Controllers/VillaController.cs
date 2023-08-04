4371662e3b6869fd0695ddb4721a7c2399b2051d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:32:20 -0500

    Section 3 - Custom Model Validations

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -27,6 +27,11 @@ namespace WhiteLagoon.Web.Controllers
         [HttpPost]
         public IActionResult Create(Villa obj)
         {
+
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Villas.Add(obj);
