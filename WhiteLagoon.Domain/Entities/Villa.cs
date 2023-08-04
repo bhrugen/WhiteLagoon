08ca31dec9b3383c40f945b1a0a53d85a6cf3563
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:28:13 -0500

    Section 3 - More Data Annotations

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -10,11 +10,14 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
         [Display(Name = "Price per night")]
+        [Range(10, 10000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1, 10)]
         public int Occupancy { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
