0380b10663668ffe905b3182460d2e3d1cc428f9
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-04 10:52:05 -0500

    Section 7 - VillaRepository Interface

---
+++ WhiteLagoon.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
+        void Add(Villa entity);
+        void Update(Villa entity);
+        void Remove(Villa entity);
+        void Save();
+
+    }
+}
