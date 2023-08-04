2e3c075a2c96877f1658de3b16b70a91e3b8c92e
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:09:45 -0500

    Section 2 - Create ApplicationDbContext

---
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
