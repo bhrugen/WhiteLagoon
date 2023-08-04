7bd222400da7e769af8847a869309f0cad4ca703
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-04 11:40:48 -0500

    Section 7 - Generic Repository

---
+++ WhiteLagoon.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ WhiteLagoon.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
