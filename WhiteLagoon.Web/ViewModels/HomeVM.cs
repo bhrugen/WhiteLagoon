d36cf03b9d987ccd21534f17f7c5fd0d46bed6f2
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-04 15:09:45 -0500

    Section 10 - Home View Model

---
+++ WhiteLagoon.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
