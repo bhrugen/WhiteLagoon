8eb103bd7b0c3dbf3f2a7abc5ace53d96ad1a4a5
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-03 19:54:39 -0500

    Section 6 - Update Post in Action

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -83,16 +83,23 @@ namespace WhiteLagoon.Web.Controllers
 
 
         [HttpPost]
-        public IActionResult Update(Villa obj)
+        public IActionResult Update(VillaNumberVM villaNumberVM)
         {
-            if (ModelState.IsValid && obj.Id > 0)
+
+            if (ModelState.IsValid)
             {
-                _db.Villas.Update(obj);
+                _db.VillaNumbers.Update(villaNumberVM.VillaNumber);
                 _db.SaveChanges();
-                TempData["success"] = "The villa has been updated successfully.";
+                TempData["success"] = "The villa Number has been updated successfully.";
                 return RedirectToAction("Index");
             }
-            return View();
+
+            villaNumberVM.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(villaNumberVM);
         }
 
 
