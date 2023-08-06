746fedfa65d40df117a20780e96182157f1d079d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-06 14:30:15 -0500

    Section 12  - Reload Partial View without refreshing page

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -28,14 +28,28 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Index(HomeVM homeVM)
         {
             homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
-            foreach(var villa in homeVM.VillaList)
+            
+            return View(homeVM);
+        }
+
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
             {
                 if (villa.Id % 2 == 0)
                 {
                     villa.IsAvailable = false;
                 }
-            } 
-            return View(homeVM);
+            }
+            HomeVM homeVM = new()
+            {
+                CheckInDate = checkInDate,
+                VillaList = villaList,
+                Nights = nights
+            };
+
+            return PartialView("_VillaList",homeVM);
         }
 
         public IActionResult Privacy()
