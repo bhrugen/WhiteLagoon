170125eca8de8ac441a2266a497b86086fad2ce9
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-06 11:15:08 -0500

    Section 11 - Add Roles in the Table

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -39,6 +39,12 @@ namespace WhiteLagoon.Web.Controllers
 
         public IActionResult Register()
         {
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
+
             return View();
         }
     }
