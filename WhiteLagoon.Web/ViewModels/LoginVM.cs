209ea20ff390ce1a8d62b65fd78a96ebf346911b
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-06 09:57:39 -0500

    Section 11 - Login and Register VM

---
+++ WhiteLagoon.Web/ViewModels/LoginVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        public string? RedirectUrl { get; set; }
+    }
+}
---
+++ WhiteLagoon.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name="Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        public string? RedirectUrl { get; set; }
+    }
+}
