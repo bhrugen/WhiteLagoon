dd30d508ad87c57b90f10f4a7f40a8645f87c8ba
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-07 12:03:50 -0500

    Section 13 - More Methods in Booking Repository

--- WhiteLagoon.Application/Common/Interfaces/IBookingRepository.cs
+++ WhiteLagoon.Application/Common/Interfaces/IBookingRepository.cs
@@ -11,5 +11,7 @@ namespace WhiteLagoon.Application.Common.Interfaces
     public interface IBookingRepository : IRepository<Booking>
     {
         void Update(Booking entity);
+        void UpdateStatus(int bookingId, string bookingStatus);
+        void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId);
     }
 }
--- WhiteLagoon.Infrastructure/Repository/BookingRepository.cs
+++ WhiteLagoon.Infrastructure/Repository/BookingRepository.cs
@@ -6,6 +6,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -23,6 +24,41 @@ namespace WhiteLagoon.Infrastructure.Repository
         public void Update(Booking entity)
         {
             _db.Bookings.Update(entity);
+        }
+
+        public void UpdateStatus(int bookingId, string bookingStatus)
+        {
+            var bookingFromDb = _db.Bookings.FirstOrDefault(m => m.Id == bookingId);
+            if (bookingFromDb != null)
+            {
+                bookingFromDb.Status = bookingStatus;
+                if (bookingStatus == SD.StatusCheckedIn)
+                {
+                    bookingFromDb.ActualCheckInDate = DateTime.Now;
+                }
+                if (bookingStatus == SD.StatusCompleted)
+                {
+                    bookingFromDb.ActualCheckOutDate = DateTime.Now;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId)
+        {
+            var bookingFromDb = _db.Bookings.FirstOrDefault(m => m.Id == bookingId);
+            if (bookingFromDb != null)
+            {
+                if (!string.IsNullOrEmpty(sessionId))
+                {
+                    bookingFromDb.StripeSessionId = sessionId;
+                }
+                if (!string.IsNullOrEmpty(paymentIntentId))
+                {
+                    bookingFromDb.StripePaymentIntentId = paymentIntentId;
+                    bookingFromDb.PaymentDate=DateTime.Now;
+                    bookingFromDb.IsPaymentSuccessful = true;
+                }
+            }
         }
     }
 }
