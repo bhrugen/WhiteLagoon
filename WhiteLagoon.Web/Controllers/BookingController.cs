bfe1a5232e1bf72510b958f69aa7bbb29a200b03
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-07 10:38:20 -0500

    Section 13 - Get Action Finalize Booking

---
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new() 
+            {
+                VillaId = villaId,
+                Villa = _unitOfWork.Villa.Get(u=>u.Id==villaId, includeProperties:"VillaAmenity"),
+                CheckInDate = checkInDate,
+                Nights = nights,
+                CheckOutDate = checkInDate.AddDays(nights),
+            };
+
+            return View(booking);
+        }
+    }
+}
