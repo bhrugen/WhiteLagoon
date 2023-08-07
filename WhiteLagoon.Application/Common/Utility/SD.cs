766ac8867f21f731fa24b47dde884304acf20d0d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-07 11:15:58 -0500

    Section 13 - Booking Status

--- WhiteLagoon.Application/Common/Utility/SD.cs
+++ WhiteLagoon.Application/Common/Utility/SD.cs
@@ -10,5 +10,13 @@ namespace WhiteLagoon.Application.Common.Utility
     {
         public const string Role_Customer = "Customer";
         public const string Role_Admin = "Admin";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusCheckedIn = "CheckedIn";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
     }
 }
