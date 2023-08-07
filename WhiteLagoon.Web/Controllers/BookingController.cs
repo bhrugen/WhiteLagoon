1162549a7265b1c582467cde8bdcf8681d251a35
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-07 15:11:03 -0500

    Section 14 - Booking Details Action Method

--- WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -119,6 +119,14 @@ namespace WhiteLagoon.Web.Controllers
             return View(bookingId);
         }
 
+        [Authorize]
+        public IActionResult BookingDetails(int bookingId)
+        {
+            Booking bookingFromDb = _unitOfWork.Booking.Get(u => u.Id == bookingId,
+             includeProperties: "User,Villa");
+
+            return View(bookingFromDb);
+        }
 
 
         #region API Calls
