8d65965c418f2091138496d36ce421850a7cc2d5
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-07 14:05:01 -0500

    Section 14 - DataTables in Depth Part 1 and 2

--- WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -16,7 +16,7 @@ namespace WhiteLagoon.Web.Controllers
         {
             _unitOfWork = unitOfWork;
         }
-
+        [Authorize]
         public IActionResult Index()
         {
             return View();
--- WhiteLagoon.Web/wwwroot/js/booking.js
+++ WhiteLagoon.Web/wwwroot/js/booking.js
@@ -14,12 +14,23 @@ function loadDataTable() {
         },
         "columns": [
             { data: 'id', "width": "5%" },
-            { data: 'name', "width": "5%" },
-            { data: 'phone', "width": "5%" },
-            { data: 'email', "width": "5%" },
-            { data: 'status', "width": "5%" },
-            { data: 'checkindate', "width": "5%" },
-            { data: 'nights', "width": "5%" }
+            { data: 'name', "width": "15%" },
+            { data: 'phone', "width": "10%" },
+            { data: 'email', "width": "15%" },
+            { data: 'status', "width": "10%" },
+            { data: 'checkInDate', "width": "10%" },
+            { data: 'nights', "width": "10%" },
+            { data: 'totalCost', render: $.fn.dataTable.render.number(',', '.', 2) , "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group">
+                        <a href="/booking/bookingDetails?bookingId=${data}" class="btn btn-outline-warning mx-2">
+                            <i class="bi bi-pencil-square"></i> Details
+                        </a>
+                    </div>`
+                }
+            }
         ]
     });
 }
