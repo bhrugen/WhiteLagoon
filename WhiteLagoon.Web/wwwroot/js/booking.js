af92d78d439d59c2654f440e78793c121d61f2b9
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-07 13:45:54 -0500

    Section 14 - DataTables

---
+++ WhiteLagoon.Web/wwwroot/js/booking.js
@@ -0,0 +1,25 @@
+﻿var dataTable;
+
+
+$(document).ready(function () {
+loadDataTable();    
+
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblBookings').DataTable({
+        "ajax": {
+            url: '/booking/getall'
+        },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "5%" },
+            { data: 'phone', "width": "5%" },
+            { data: 'email', "width": "5%" },
+            { data: 'status', "width": "5%" },
+            { data: 'checkindate', "width": "5%" },
+            { data: 'nights', "width": "5%" }
+        ]
+    });
+}
