631a310a53575c7e1217df1c465c57e776e366c9
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-12 12:57:50 -0500

    better design and functional

---
+++ WhiteLagoon.Web/wwwroot/js/Dashboard/radialCharts.js
@@ -0,0 +1,31 @@
+﻿
+// Function to load the RadialBar chart
+function loadRadialBarChart(id, data) {
+
+    var options = {
+        chart: {
+            type: 'radialBar',
+            width: 90,
+            height: 90,
+            sparkline: {
+                enabled: true
+            },
+            offsetY: -10,
+        },
+        series: data.series,
+        plotOptions: {
+            radialBar: {
+                dataLabels: {
+                    value: {
+                        offsetY: -10,
+                    }
+                }
+            }
+        },
+        labels: [""]
+    };
+
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+}
+
