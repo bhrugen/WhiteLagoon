7245691860b99ba237bb303f4f44e24849128e9d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-12 12:58:12 -0500

    color options

--- WhiteLagoon.Web/wwwroot/js/Dashboard/radialCharts.js
+++ WhiteLagoon.Web/wwwroot/js/Dashboard/radialCharts.js
@@ -1,8 +1,12 @@
 ﻿
+
 // Function to load the RadialBar chart
 function loadRadialBarChart(id, data) {
-
+    var chartColors = getChartColorsArray(id);
     var options = {
+        fill: {
+            colors: chartColors
+        },
         chart: {
             type: 'radialBar',
             width: 90,
@@ -18,6 +22,7 @@ function loadRadialBarChart(id, data) {
                 dataLabels: {
                     value: {
                         offsetY: -10,
+                        color: chartColors[0],
                     }
                 }
             }
@@ -29,3 +34,20 @@ function loadRadialBarChart(id, data) {
     chart.render();
 }
 
+
+function getChartColorsArray(chartId) {
+    if (document.getElementById(chartId) !== null) {
+        var colors = document.getElementById(chartId).getAttribute("data-colors");
+        if (colors) {
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    if (color) return color;
+                    else return newValue;;
+                }
+            });
+        }
+    }
+}
