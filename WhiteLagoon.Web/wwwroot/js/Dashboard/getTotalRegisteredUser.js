570caf28f5ac0db675af276267b5fdff404e8519
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-12 13:05:34 -0500

    radial functional

---
+++ WhiteLagoon.Web/wwwroot/js/Dashboard/getTotalRegisteredUser.js
@@ -0,0 +1,38 @@
+﻿
+$(document).ready(function () {
+    loadTotalRegisteredUserChart();
+});
+
+function loadTotalRegisteredUserChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: '/Dashboard/GetRegisteredUserChartData',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+            // Handle the response data
+            document.querySelector("#spanTotalRegisteredUserCount").innerHTML = data.totalCount;
+            var sectionUserRegisterdRatioHtml = document.createElement("span");
+            if (data.hasRatioIncreased) {
+                sectionUserRegisterdRatioHtml.className = "text-success me-1";
+                sectionUserRegisterdRatioHtml.innerHTML = "<i class='bi bi-arrow-up-right-circle me-1'></i><span>+" + data.increaseDecreaseAmount + "</span>";
+            }
+            else {
+                sectionUserRegisterdRatioHtml.className = "text-danger me-1";
+                sectionUserRegisterdRatioHtml.innerHTML = "<i class='bi bi-arrow-down-right-circle me-1'></i><span>+" + data.increaseDecreaseAmount + "</span>";
+            }
+            document.querySelector("#sectionUserRegisterdRatio").append(sectionUserRegisterdRatioHtml);
+            document.querySelector("#sectionUserRegisterdRatio").append("since last month");
+
+            loadRadialBarChart("totalRegisteredUserRadialChart", data);
+
+            $(".chart-spinner").hide();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.error('Error:', errorThrown);
+        }
+    });
+}
+
---
+++ WhiteLagoon.Web/wwwroot/js/Dashboard/getTotalRevenue.js
@@ -0,0 +1,38 @@
+﻿
+$(document).ready(function () {
+    loadTotalRevenueChart();
+});
+
+function loadTotalRevenueChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: '/Dashboard/GetTotalRevenueChartData',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+            // Handle the response data
+            document.querySelector("#spanTotalRevenueCount").innerHTML = data.totalCount;
+            var sectionRevenueRatioHtml = document.createElement("span");
+            if (data.hasRatioIncreased) {
+                sectionRevenueRatioHtml.className = "text-success me-1";
+                sectionRevenueRatioHtml.innerHTML = "<i class='bi bi-arrow-up-right-circle me-1'></i><span>+" + data.increaseDecreaseAmount + "</span>";
+            }
+            else {
+                sectionRevenueRatioHtml.className = "text-danger me-1";
+                sectionRevenueRatioHtml.innerHTML = "<i class='bi bi-arrow-down-right-circle me-1'></i><span>+" + data.increaseDecreaseAmount + "</span>";
+            }
+            document.querySelector("#sectionRevenueRatio").append(sectionRevenueRatioHtml);
+            document.querySelector("#sectionRevenueRatio").append("since last month");
+
+            loadRadialBarChart("totalRevenueRadialChart", data);
+
+            $(".chart-spinner").hide();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.error('Error:', errorThrown);
+        }
+    });
+}
+
