e910de63ca56f5997e802421bfe1584759fac0b1
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-12 14:17:05 -0500

    Dashboard Service Interface

--- WhiteLagoon.Application/Services/Interfaces/IDashboardService.cs
+++ WhiteLagoon.Application/Services/Interfaces/IDashboardService.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhiteLagoon.Shared.ViewModels;
 
 namespace WhiteLagoon.Application.Services.Interfaces
 {
     public interface IDashboardService
     {
-       //Access Dashboard VM
+        Task<RadialBarChartVM> GetBookingsChartDataAsync();
+        Task<RadialBarChartVM> GetRevenueChartDataAsync();
+        Task<RadialBarChartVM> GetRegisteredUserChartDataAsync();
+        Task<DashboardLineChartVM> GetMemberAndBookingChartDataAsync();
+        Task<DashboardPieChartVM> GetBookingPieChartDataAsync();
     }
 }
