e7c3ccbc27152af37ac3fac4b383ea9b70cd0f0a
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-13 12:56:55 -0500

    Section 16 - RadialBarChart VM

---
+++ WhiteLagoon.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace WhiteLagoon.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal IncreaseDecreaseAmount { get; set; }
+        public bool HasRatioIncreased { get; set; }
+        public int[] Series { get; set; }
+    }
+}
