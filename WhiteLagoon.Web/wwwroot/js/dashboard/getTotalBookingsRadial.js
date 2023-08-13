9477af577666d30e5e4a854ad15511f3c4f0d25a
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-13 13:38:03 -0500

    Section 16 - Load Dashboard Total Bookings Data

---
+++ WhiteLagoon.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -0,0 +1,31 @@
+﻿
+$(document).ready(function () {
+    loadTotalBookingRadialChart();
+});
+
+function loadTotalBookingRadialChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetTotalBookingRadialChartData",
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            document.querySelector("#spanTotalBookingCount").innerHTML = data.totalCount;
+
+            var sectionCurrentCount = document.createElement("span");
+            if (data.hasRatioIncreased) {
+                sectionCurrentCount.className = "text-success me-1";
+                sectionCurrentCount.innerHTML = '<i class="bi bi-arrow-up-right-circle me-1"></i> <span> ' + data.countInCurrentMonth + '</span>';
+            }
+            else {
+                sectionCurrentCount.className = "text-danger me-1";
+                sectionCurrentCount.innerHTML = '<i class="bi bi-arrow-down-right-circle me-1"></i> <span> ' + data.countInCurrentMonth + '</span>';
+            }
+
+            document.querySelector("#sectionBookingCount").append(sectionCurrentCount);
+            document.querySelector("#sectionBookingCount").append("since last month");
+            $(".chart-spinner").hide();
+        }
+    });
+}
