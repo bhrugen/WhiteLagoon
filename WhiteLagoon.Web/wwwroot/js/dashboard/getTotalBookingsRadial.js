ba1d9648c452e2040bca68923e274f7a1ee49794
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-13 14:07:53 -0500

    Section 16 - Better Code Design

--- WhiteLagoon.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
+++ WhiteLagoon.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -33,31 +33,3 @@ function loadTotalBookingRadialChart() {
     });
 }
 
-
-function loadRadialBarChart(id, data) {
-    var options = {
-        chart: {
-            height: 90,
-            width: 90,
-            type: "radialBar",
-            sparkline: {
-                enabled: true
-            },
-            offsetY: -10,
-        },
-        series: data.series,
-        plotOptions: {
-            radialBar: {
-                dataLabels: {
-                    value: {
-                        offsetY: -10,
-                    }
-                }
-            }
-        },
-        labels:[""]
-    };
-    var chart = new ApexCharts(document.querySelector("#"+id), options);
-    chart.render();
-
-}
---
+++ WhiteLagoon.Web/wwwroot/js/dashboard/radailChart.js
@@ -0,0 +1,28 @@
+﻿
+function loadRadialBarChart(id, data) {
+    var options = {
+        chart: {
+            height: 90,
+            width: 90,
+            type: "radialBar",
+            sparkline: {
+                enabled: true
+            },
+            offsetY: -10,
+        },
+        series: data.series,
+        plotOptions: {
+            radialBar: {
+                dataLabels: {
+                    value: {
+                        offsetY: -10,
+                    }
+                }
+            }
+        },
+        labels: [""]
+    };
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+
+}
