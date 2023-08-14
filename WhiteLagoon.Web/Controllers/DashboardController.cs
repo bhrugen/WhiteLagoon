ffd91f64bc3b25f1a43efc1e1387d9da310568af
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-14 08:07:39 -0500

    Section 16 - Endpoint for Pie Chart Action

--- WhiteLagoon.Web/Controllers/DashboardController.cs
+++ WhiteLagoon.Web/Controllers/DashboardController.cs
@@ -66,6 +66,26 @@ namespace WhiteLagoon.Web.Controllers
             return Json(GetRadialCartDataModel(totalRevenue, countByCurrentMonth, countByPreviousMonth));
         }
 
+        public async Task<IActionResult> GetBookingPieChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.BookingDate >= DateTime.Now.AddDays(-30) &&
+            (u.Status != SD.StatusPending || u.Status == SD.StatusCancelled));
+
+            var customerWithOneBooking = totalBookings.GroupBy(b=>b.UserId).Where(x=>x.Count()==1).Select(x=>x.Key).ToList(); 
+
+            int bookingsByNewCustomer = customerWithOneBooking.Count();
+            int bookingsByReturningCustomer = totalBookings.Count() - bookingsByNewCustomer;
+
+            PieChartVM pieChartVM = new()
+            {
+                Labels = new string[] { "New Customer Bookings", "Returning Customer Bookings" },
+                Series = new decimal[] { bookingsByNewCustomer, bookingsByReturningCustomer }
+            };
+
+            return Json(pieChartVM);
+        }
+
+
 
         private static RadialBarChartVM GetRadialCartDataModel(int totalCount, double currentMonthCount, double prevMonthCount)
         {
