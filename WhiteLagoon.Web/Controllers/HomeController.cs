b18b26235ba090cc9cf5f34724f751cb0712b1e9
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-14 14:22:11 -0500

    Section 17 - Download PPT

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Syncfusion.Presentation;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Web.ViewModels;
@@ -9,9 +10,12 @@ namespace WhiteLagoon.Web.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -49,6 +53,61 @@ namespace WhiteLagoon.Web.Controllers
             };
 
             return PartialView("_VillaList",homeVM);
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePPTExport(int id)
+        {
+            var villa = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").FirstOrDefault(x => x.Id == id);
+            if (villa is null)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+            string basePath = _webHostEnvironment.WebRootPath;
+            string filePath = basePath + @"/Exports/ExportVillaDetails.pptx";
+
+
+            using IPresentation presentation = Presentation.Open(filePath);
+
+            ISlide slide = presentation.Slides[0];
+
+
+            IShape? shape = slide.Shapes.FirstOrDefault(u=>u.ShapeName== "txtVillaName") as IShape;
+            if(shape is not null)
+            {
+                shape.TextBody.Text = villa.Name;
+            }
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaDescription") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = villa.Description;
+            }
+
+
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtOccupancy") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Max Occupancy : {0} adults", villa.Occupancy);
+            }
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaSize") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("Villa Size: {0} sqft", villa.Sqft);
+            }
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtPricePerNight") as IShape;
+            if (shape is not null)
+            {
+                shape.TextBody.Text = string.Format("USD {0}/night", villa.Price.ToString("C"));
+            }
+
+            MemoryStream memoryStream = new();
+            presentation.Save(memoryStream);
+            memoryStream.Position = 0;
+            return File(memoryStream,"application/pptx","villa.pptx");
+
+
         }
 
         public IActionResult Privacy()
