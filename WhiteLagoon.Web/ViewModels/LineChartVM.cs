61c10f0b90d7ae83f6abd3d3635c26e6db220aca
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-14 08:48:16 -0500

    Section 16 - LineChart View Model

---
+++ WhiteLagoon.Web/ViewModels/LineChartVM.cs
@@ -0,0 +1,14 @@
+﻿namespace WhiteLagoon.Web.ViewModels
+{
+    public class LineChartVM
+    {
+        public List<ChartData> Series { get; set; }
+        public string[] Categories { get; set; }
+    }
+
+    public class ChartData
+    {
+        public string Name { get; set; }
+        public int[] Data { get; set; }
+    }
+}
