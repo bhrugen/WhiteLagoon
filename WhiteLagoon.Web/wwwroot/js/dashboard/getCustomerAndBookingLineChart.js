7fe3d4352c05e5a53f98f9f1efdcf967a0126a33
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-14 09:40:43 -0500

    Section 16 - Line Chart Part 1

---
+++ WhiteLagoon.Web/wwwroot/js/dashboard/getCustomerAndBookingLineChart.js
@@ -0,0 +1,47 @@
+﻿
+$(document).ready(function () {
+    loadCustomerBookingPieChart();
+});
+
+function loadCustomerBookingPieChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetMemberAndBookingLineChartData",
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+      
+            loadLineChart("newMembersAndBookingsLineChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    });
+}
+
+function loadLineChart(id, data) {
+    var chartColors = getChartColorsArray(id);
+    var options = {
+        colors: chartColors,
+        series: data.series,
+        
+        chart: {
+            height: 350,
+            type: 'line',
+        },
+        stroke: {
+            show: false
+        },
+        markers: {
+            size: 0,
+            hover: {
+                sizeOffset: 6
+            }
+        },
+        xaxis: {
+            categories: data.categories,
+        },
+    };
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+}
