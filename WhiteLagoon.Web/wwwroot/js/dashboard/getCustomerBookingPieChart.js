d91d3ce3eb0421b2cb31dc2b7f8d50ec12de79bd
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-08-14 08:37:05 -0500

    Section 16 - Pie chart in action

---
+++ WhiteLagoon.Web/wwwroot/js/dashboard/getCustomerBookingPieChart.js
@@ -0,0 +1,46 @@
+﻿
+$(document).ready(function () {
+    loadCustomerBookingPieChart();
+});
+
+function loadCustomerBookingPieChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetBookingPieChartData",
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+      
+            loadPieChart("customerBookingsPieChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    });
+}
+
+function loadPieChart(id, data) {
+    var chartColors = getChartColorsArray(id);
+    var options = {
+        colors: chartColors,
+        series: data.series,
+        labels: data.labels,
+        chart: {
+            width: 380,
+            type: 'pie',
+        },
+        stroke: {
+            show: false
+        },
+        legend: {
+            position: 'bottom',
+            horizontalAlign: 'center',
+            labels: {
+                colors: "#fff",
+                useSeriesColors: true
+            },
+        },
+    };
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+}
