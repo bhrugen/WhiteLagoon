0cc29da5c281e956c4bd53655d359784047d97e8
Bhrugen Patel <bpatel@technomic.com>
2023-08-15 12:27:38 -0500

    Email Sender Interface

---
+++ WhiteLagoon.Application/Common/Email/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Models;
+
+namespace WhiteLagoon.Application.Common.Email
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(EmailMessage email);
+    }
+}
---
+++ WhiteLagoon.Application/Common/Models/EmailMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Application.Common.Models
+{
+    public class EmailMessage
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
