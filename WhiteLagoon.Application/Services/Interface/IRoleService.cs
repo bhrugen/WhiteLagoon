9efa8af736b4ed1aa0ffb392e69f174576838f63
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-09-08 11:42:35 -0500

    roleservice

---
+++ WhiteLagoon.Application/Services/Interface/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Application.Services.Interface
+{
+    public interface IRoleService
+    {
+        Task<bool> EnsureRolesExistAsync();
+        IEnumerable<SelectListItem> GetRoleList();
+    }
+}
