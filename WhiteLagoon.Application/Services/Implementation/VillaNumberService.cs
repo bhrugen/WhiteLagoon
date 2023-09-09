7a2d3170a7df3416ebf47c1473460bb59bfce24b
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-09-09 14:47:14 -0500

    Section 20 - VillaNumber Service

---
+++ WhiteLagoon.Application/Services/Implementation/VillaNumberService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Services.Interface;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Implementation
+{
+    public class VillaNumberService : IVillaNumberService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VillaNumberService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void CreateVillaNumber(VillaNumber villaNumber)
+        {
+            _unitOfWork.VillaNumber.Add(villaNumber);
+            _unitOfWork.Save();
+        }
+
+        public bool DeleteVillaNumber(int id)
+        {
+            try
+            {
+                VillaNumber? objFromDb = _unitOfWork.VillaNumber.Get(u => u.Villa_Number == id);
+                if (objFromDb is not null)
+                {
+                    _unitOfWork.VillaNumber.Remove(objFromDb);
+                    _unitOfWork.Save();
+                    return true;
+                }
+               return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }   
+        }
+
+        public IEnumerable<VillaNumber> GetAllVillaNumbers()
+        {
+            return _unitOfWork.VillaNumber.GetAll();
+        }
+
+        public VillaNumber GetVillaNumberById(int id)
+        {
+            return _unitOfWork.VillaNumber.Get(u => u.Villa_Number == id);
+        }
+
+        public void UpdateVillaNumber(VillaNumber villaNumber)
+        {
+            _unitOfWork.VillaNumber.Update(villaNumber);
+            _unitOfWork.Save();
+        }
+    }
+}
---
+++ WhiteLagoon.Application/Services/Interface/IVillaNumberService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Interface
+{
+    public interface IVillaNumberService
+    {
+        IEnumerable<VillaNumber> GetAllVillaNumbers();
+        VillaNumber GetVillaNumberById(int id);
+        void CreateVillaNumber(VillaNumber villaNumber);
+        void UpdateVillaNumber(VillaNumber villaNumber);
+        bool DeleteVillaNumber(int id);
+    }
+}
