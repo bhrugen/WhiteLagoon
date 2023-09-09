c0e92e3ee7d88722c2ad38c9bfffac6c1efbb9e4
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-09-09 14:25:57 -0500

    Section 20 - Create Villa Service Interface

---
+++ WhiteLagoon.Application/Services/Interface/IVillaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Interface
+{
+    public interface IVillaService
+    {
+        IEnumerable<Villa> GetAllVillas();
+        Villa GetVillaById(int id);
+        void CreateVilla(Villa villa);
+        void UpdateVilla(Villa villa);
+        bool DeleteVilla(int id);
+    }
+}
