de5ea85c17639b97ebf0ccc180fb474d432bf73a
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-09-10 14:57:58 -0500

    Section 20 - Booking Interface Part 1

---
+++ WhiteLagoon.Application/Services/Interface/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Interface
+{
+    public interface IBookingService
+    {
+        void CreateBooking(Booking booking);
+        Booking GetBookingById(int bookingId);
+        IEnumerable<Booking> GetAllBookings(string userId="",string? statusFilterList="");
+    }
+}
