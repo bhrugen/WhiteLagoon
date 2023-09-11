371d467759cf70246030673b03ca0f4c0f3fc6e2
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-09-10 22:54:33 -0500

    Section 21 - Email in Action

--- WhiteLagoon.Infrastructure/Emails/EmailService.cs
+++ WhiteLagoon.Infrastructure/Emails/EmailService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using WhiteLagoon.Application.Contract;
@@ -9,11 +13,26 @@ namespace WhiteLagoon.Infrastructure.Emails
 {
     public class EmailService : IEmailService
     {
-
-
-        public Task<bool> SendEmailAsync(string email, string subject, string message)
+        private readonly string _sendGridKey;
+        public EmailService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _sendGridKey = configuration["SendGrid:Key"];
+        }
+
+
+        public async Task<bool> SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SendGridClient(_sendGridKey);
+            var from = new EmailAddress("hello@dotnetmastery.com", "DotNetMastery - White Lagoon");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
+            var response = await client.SendEmailAsync(msg);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
