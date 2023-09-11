e939e3bd674b8123df90787594b6a40e57d9492f
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-09-10 22:42:15 -0500

    Section 21 - Create IEmailService

---
+++ WhiteLagoon.Infrastructure/Emails/EmailService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Contract;
+
+namespace WhiteLagoon.Infrastructure.Emails
+{
+    public class EmailService : IEmailService
+    {
+
+
+        public Task<bool> SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
